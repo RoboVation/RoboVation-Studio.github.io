650d3d6b37408c2b4492d8ea81f376ad7929a0c0
RoboVation <theprogramming.04@gmail.com>
2025-02-12 09:46:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-<script>
-  document.getElementById("close-alert").addEventListener("click", function() {
-    document.getElementById("giveaway-alert").style.display ="none"});
-</script>
+const videoList = document.getElementById("video-list");
+
+videos.forEach(video => {
+  const videoItem = document.createElement("div");
+  videoItem.classList.add("video-item");
+
+  // Create buttons for each link
+  const buttonsHTML = video.links
+    ? video.links
+        .map(
+          link => `
+        <a href="${link.url}" class="explore-button" target="_blank">
+          <button>${link.text}</button>
+        </a>
+      `
+        )
+        .join("")
+    : "";
+
+  videoItem.innerHTML = `
+    <div class="text-content">
+      <h3>${video.title}</h3>
+      <p>${video.description}</p>
+      ${buttonsHTML}
+    </div>
+    <div class="video-wrapper">
+      <iframe width="500" height="315" src="https://www.youtube.com/embed/${video.youtubeId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  `;
+  videoList.appendChild(videoItem);
+});
